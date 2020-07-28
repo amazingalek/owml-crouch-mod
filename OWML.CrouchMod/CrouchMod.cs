645389f110ea687f17aa8635cdcc33a4815aafcf
AmazingAlek <alek.ntnu@gmail.com>
2020-07-28 19:43:43 +0200

    Config (#3)

--- OWML.CrouchMod/CrouchMod.cs
+++ OWML.CrouchMod/CrouchMod.cs
@@ -1,4 +1,5 @@
-﻿using OWML.Common;
+﻿using System.Collections.Generic;
+using OWML.Common;
 using OWML.ModHelper;
 using OWML.ModHelper.Events;
 using UnityEngine;
@@ -7,16 +8,38 @@ namespace OWML.CrouchMod
 {
     public class CrouchMod : ModBehaviour
     {
-        private const string Combination = "C";
-
         private PlayerCharacterController _playerCharacterController;
         private float? _crouchStartTime;
         private IModInputCombination _crouchCombo;
 
+        public override void Configure(IModConfig config)
+        {
+            if (_crouchCombo != null)
+            {
+                ModHelper.Input.UnregisterCombination(_crouchCombo);
+            }
+            var combination = config.GetSettingsValue<string>("Crouch combination");
+            _crouchCombo = ModHelper.Input.RegisterCombination(this, "Crouch", combination) ?? TemporaryHack("Crouch");
+        }
+
+        private IModInputCombination TemporaryHack(string comboName)
+        {
+            var comboRegistry = ModHelper.Input.GetValue<Dictionary<long, HashSet<IModInputCombination>>>("_comboRegistry");
+            foreach (var foo in comboRegistry)
+            {
+                foreach (var bar in foo.Value)
+                {
+                    if (bar.Name == comboName)
+                    {
+                        return bar;
+                    }
+                }
+            }
+            return null;
+        }
+
         private void Start()
         {
-            _crouchCombo = ModHelper.Input.RegisterCombination(this, "Crouch", Combination);
-
             ModHelper.Events.Player.OnPlayerAwake += OnPlayerAwake;
         }
 
