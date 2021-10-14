cac7763c9de3e68d108e42286fc3d22a7dfc1aaf
Aleksander Waage <alek.ntnu@gmail.com>
2021-10-14 22:10:31 +0200

    v2 support

--- OWML.CrouchMod/CrouchMod.cs
+++ OWML.CrouchMod/CrouchMod.cs
@@ -1,90 +1,61 @@
-﻿using System.Collections.Generic;
-using OWML.Common;
-using OWML.ModHelper;
-using OWML.ModHelper.Events;
+﻿using OWML.ModHelper;
+using OWML.Utils;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace OWML.CrouchMod
 {
-    public class CrouchMod : ModBehaviour
-    {
-        private PlayerCharacterController _playerCharacterController;
-        private float? _crouchStartTime;
-        private IModInputCombination _crouchCombo;
+	public class CrouchMod : ModBehaviour
+	{
+		private PlayerCharacterController _playerCharacterController;
+		private float? _crouchStartTime;
 
-        public override void Configure(IModConfig config)
-        {
-            if (_crouchCombo != null)
-            {
-                ModHelper.Input.UnregisterCombination(_crouchCombo);
-            }
-            var combination = config.GetSettingsValue<string>("Crouch combination");
-            _crouchCombo = ModHelper.Input.RegisterCombination(this, "Crouch", combination) ?? TemporaryHack("Crouch");
-        }
+		public void Start()
+		{
+			ModHelper.Events.Player.OnPlayerAwake += OnPlayerAwake;
+		}
 
-        private IModInputCombination TemporaryHack(string comboName)
-        {
-            var comboRegistry = ModHelper.Input.GetValue<Dictionary<long, HashSet<IModInputCombination>>>("_comboRegistry");
-            foreach (var foo in comboRegistry)
-            {
-                foreach (var bar in foo.Value)
-                {
-                    if (bar.Name == comboName)
-                    {
-                        return bar;
-                    }
-                }
-            }
-            return null;
-        }
+		private void OnPlayerAwake(PlayerBody playerBody)
+		{
+			_playerCharacterController = playerBody.GetComponent<PlayerCharacterController>();
+		}
 
-        private void Start()
-        {
-            ModHelper.Events.Player.OnPlayerAwake += OnPlayerAwake;
-        }
+		public void Update()
+		{
+			if (_playerCharacterController == null)
+			{
+				return;
+			}
+			HandleInput();
+			SetCrouchValue();
+		}
 
-        private void OnPlayerAwake(PlayerBody playerBody)
-        {
-            _playerCharacterController = playerBody.GetComponent<PlayerCharacterController>();
-        }
+		private void HandleInput()
+		{
+			if (Keyboard.current[Key.C].wasPressedThisFrame)
+			{
+				_crouchStartTime = Time.realtimeSinceStartup;
+			}
+			else if (Keyboard.current[Key.C].wasReleasedThisFrame &&
+					 !OWInput.IsPressed(InputLibrary.jump))
+			{
+				_crouchStartTime = null;
+				_playerCharacterController.SetValue("_jumpChargeTime", 0);
+			}
+			else if (OWInput.IsNewlyReleased(InputLibrary.jump))
+			{
+				_crouchStartTime = null;
+			}
+		}
 
-        private void Update()
-        {
-            if (_playerCharacterController == null)
-            {
-                return;
-            }
-            HandleInput();
-            SetCrouchValue();
-        }
-
-        private void HandleInput()
-        {
-            if (ModHelper.Input.IsNewlyPressedExact(_crouchCombo))
-            {
-                _crouchStartTime = Time.realtimeSinceStartup;
-            }
-            else if (ModHelper.Input.WasNewlyReleased(_crouchCombo) &&
-                     !OWInput.IsPressed(InputLibrary.jump))
-            {
-                _crouchStartTime = null;
-                _playerCharacterController.SetValue("_jumpChargeTime", 0);
-            }
-            else if (OWInput.IsNewlyReleased(InputLibrary.jump))
-            {
-                _crouchStartTime = null;
-            }
-        }
-
-        private void SetCrouchValue()
-        {
-            if (_crouchStartTime == null)
-            {
-                return;
-            }
-            var jumpChargeTime = Time.realtimeSinceStartup - _crouchStartTime.Value;
-            _playerCharacterController.SetValue("_jumpChargeTime", jumpChargeTime);
-        }
-
-    }
+		private void SetCrouchValue()
+		{
+			if (_crouchStartTime == null)
+			{
+				return;
+			}
+			var jumpChargeTime = Time.realtimeSinceStartup - _crouchStartTime.Value;
+			_playerCharacterController.SetValue("_jumpChargeTime", jumpChargeTime);
+		}
+	}
 }
